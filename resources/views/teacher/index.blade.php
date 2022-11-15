674854f9207a38359ca70c8ebf8a567754d4e33a
chikaciau <chikaciau2002@gmail.com>
2022-11-15 20:30:07 +0700

    tugas 2 tambahin table

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,18 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
 <h2>Teacher List</h2>
-    <ol>
+
+<table class="table table-primary table-striped">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nama</th>
+      </tr>
+    </thead>
+    <tbody>
         @forelse ($teachers as $teacher)
-        <li>{{ $teacher->name }}</li>
+        <tr>
+            <th scope="row">{{ $loop->iteration }}</th>
+            <td>{{ $teacher->name }}</td>
+        </tr>
         @empty
-            <p>No users</p>
+        <p>No users</p>
         @endforelse
-    </ol>
+    </tbody>
+    <tfoot>
+        @for($a=1; $a<=$teachers->count(); $a++)
+            @php $count++ @endphp
+        @endfor
+        <p>Jumlah Guru: {{ $count }}</p>
+    </tfoot>
+  </table>
 
-    @for($a=1; $a<=$teachers->count(); $a++)
-        @php $count++ @endphp
-    @endfor
-
-    <p>Jumlah Guru: {{ $count }}</p>
+</div>
 @endsection
